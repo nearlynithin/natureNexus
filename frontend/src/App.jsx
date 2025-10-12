d3fc40e0a2bcb73589c057e7225678cafa667ba8
nearlynithin <nearlynithin@gmail.com>
2025-10-12 18:17:09 +0530

    feat: added a basic message interface

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,73 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./App.css";
 
-function App() {
-    const [count, setCount] = useState(0);
+function Messenger() {
+  const [connStatus, setConnStatus] = useState(false);
+  const [message, setMessage] = useState("");
+  const [msgList, setMsgList] = useState([]);
+  const socketRef = useRef(null);
 
-    return (
-        <>
-            <h1>Nature Nexus 🌱</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-            </div>
-        </>
-    );
+  useEffect(() => {
+    const socket = new WebSocket(`ws://localhost:3000`);
+    socketRef.current = socket;
+
+    socket.addEventListener("open", (event) => {
+      console.log("Websocket connection opened");
+      setConnStatus(true);
+    });
+
+    socket.addEventListener("close", (event) => {
+      console.log("Websocket connection closed ");
+      setConnStatus(false);
+    });
+
+    socket.addEventListener("message", (event) => {
+      setMsgList((prev) => [...prev, event.data]);
+    });
+
+    return () => socket.close();
+  }, []);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (socketRef.current && socketRef.current.readyState == WebSocket.OPEN) {
+      socketRef.current.send(message);
+      setMessage("");
+    }
+  }
+
+  return (
+    <>
+      <div>
+        <h2>{connStatus ? "Connected" : "Disconnected"}</h2>
+        {msgList.length > 0 && <h4>Messages:</h4>}
+        <ul>
+          {msgList.map((msg, index) => (
+            <li key={index}>{msg}</li>
+          ))}
+        </ul>
+      </div>
+      <form onSubmit={handleSubmit} type="submit">
+        <input
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Enter your message"
+        />
+        <button type="submit">send</button>
+      </form>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <h1>Nature Nexus 🌱</h1>
+      <div className="card">
+        <Messenger />
+      </div>
+    </>
+  );
 }
 
 export default App;
