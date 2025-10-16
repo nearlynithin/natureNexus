6f922777ba780d5c844f1b0f588937965e5873fc
nearlynithin <nearlynithin@gmail.com>
2025-10-16 19:55:31 +0530

    feat: react-router setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,71 +1,15 @@
-import { useState, useEffect, useRef } from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Messenger from "./components/messenger";
 import "./App.css";
-
-function Messenger() {
-  const [connStatus, setConnStatus] = useState(false);
-  const [message, setMessage] = useState("");
-  const [msgList, setMsgList] = useState([]);
-  const socketRef = useRef(null);
-
-  useEffect(() => {
-    const socket = new WebSocket(`ws://localhost:3000`);
-    socketRef.current = socket;
-
-    socket.addEventListener("open", (event) => {
-      console.log("Websocket connection opened");
-      setConnStatus(true);
-    });
-
-    socket.addEventListener("close", (event) => {
-      console.log("Websocket connection closed ");
-      setConnStatus(false);
-    });
-
-    socket.addEventListener("message", (event) => {
-      setMsgList((prev) => [...prev, event.data]);
-    });
-
-    return () => socket.close();
-  }, []);
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    if (socketRef.current && socketRef.current.readyState == WebSocket.OPEN) {
-      socketRef.current.send(message);
-      setMessage("");
-    }
-  }
-
-  return (
-    <>
-      <div>
-        <h2>{connStatus ? "Connected" : "Disconnected"}</h2>
-        {msgList.length > 0 && <h4>Messages:</h4>}
-        <ul>
-          {msgList.map((msg, index) => (
-            <li key={index}>{msg}</li>
-          ))}
-        </ul>
-      </div>
-      <form onSubmit={handleSubmit} type="submit">
-        <input
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          placeholder="Enter your message"
-        />
-        <button type="submit">send</button>
-      </form>
-    </>
-  );
-}
 
 function App() {
   return (
     <>
-      <h1>Nature Nexus 🌱</h1>
-      <div className="card">
-        <Messenger />
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/message" element={<Messenger />} />
+      </Routes>
     </>
   );
 }
---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <>
+      <h1>Nature Nexus 🌱</h1>
+      <h3>This is the home page</h3>
+    </>
+  );
+}
+
+export default Home;
