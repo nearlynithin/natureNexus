4b6bcfe82eaa15be16829f5d4e351034e461c36b
nearlynithin <nearlynithin@gmail.com>
2025-10-28 19:49:35 +0530

    feat: draw positions on map using leaflet

--- frontend/src/pages/location.jsx
+++ frontend/src/pages/location.jsx
@@ -1,21 +1,52 @@
 import { useState, useEffect } from "react";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+const markerIcon = new L.Icon({
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+});
 
 function Location() {
   const [coords, setCoords] = useState({ latitude: null, longitude: null });
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition((pos) => {
-      const { latitude, longitude } = pos.coords;
-      setCoords({ latitude: latitude, longitude: longitude });
-    });
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        const { latitude, longitude } = pos.coords;
+        setCoords({ latitude, longitude });
+      },
+      (err) => console.error("Geolocation error:", err),
+      { enableHighAccuracy: true },
+    );
   }, []);
 
+  if (coords.latitude === null || coords.longitude === null)
+    return <p>Getting your location...</p>;
+
   return (
-    <>
-      <h1>Current Position:</h1>
-      <h3>Latitude: {coords.latitude}</h3>
-      <h3>Longitude : {coords.longitude}</h3>
-    </>
+    <div
+      style={{
+        height: "400px",
+        width: "400px",
+        borderRadius: "12px",
+        overflow: "hidden",
+      }}
+    >
+      <MapContainer
+        center={[coords.latitude, coords.longitude]}
+        zoom={15}
+        style={{ height: "100%", width: "100%" }}
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Marker
+          position={[coords.latitude, coords.longitude]}
+          icon={markerIcon}
+        />
+      </MapContainer>
+    </div>
   );
 }
 
